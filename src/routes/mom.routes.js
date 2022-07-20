5a150080ad9e28de2c032269d4a71fea4eae0d1d
Joni-Kurki <joni.kurki@gmail.com>
2022-07-20 23:27:41 +0300

    -init template

---
+++ src/routes/mom.routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router();
+
+//router.use("/getAssetsROI", getAssetsROI);
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import momRouter from "./routes/mom.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+var corsOptions = {
+	origin: process.env.CORS 
+	//|| "https://www.niftykicksfactory.link"
+}
+
+console.log(corsOptions) 
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send("app works");
+});
+
+app.use("/api/mom/", momRouter);
+
+app.listen(process.env.PORT || 3001, () => console.log('Up and running... 🚀'));
